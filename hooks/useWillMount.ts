c1e4fe5f81ef55edc42b82601e594ecdd7e34d97
Raffaele Filiberti <raffaele.filiberti@gmail.com>
2021-08-09 14:46:45 +0200

    refactor(hooks): remove common hooks

--- hooks/useWillMount.ts
+++
@@ -1,9 +0,0 @@
-import { useRef } from 'react';
-
-export default function useWillMount(cb: () => void): void {
-  const isMounted = useRef(false);
-  if (!isMounted.current) {
-    isMounted.current = true;
-    cb();
-  }
-}
